90fad3cbdfeeca7cc8b87c997dfd115571d544c7
francesco1203 <francesco.schettini12@gmail.com>
2024-12-29 17:07:35 +0100

    GA commento

--- GA.py
+++ GA.py
@@ -8,10 +8,7 @@ import tkinter as tk
 from tkinter import ttk
 
 
-#parametri dell'ottimizzazione
-Ntot = 100          #numero capienza database
-N = 20              #numero farmaci utilizzati
-
+### DEFINIZIONE INTERFACCIA GRAFICA E INSERIMENTO DATI
 
 def submit_and_close():
     global nome_cognome, misura_glicemia, misura_pressione_sistolica, misura_pressione_diastolica, ottimizza_costo, salute_fegato_reni_perc
@@ -24,7 +21,7 @@ def submit_and_close():
     ottimizza_costo = bool(checkbox_var.get())
     salute_fegato_reni_perc = int(percentuale_var.get()) /100
 
-    # Mostra i valori nella console o elabora ulteriormente
+    # Mostra i valori nella console
     print(f"Nome e cognome: {nome_cognome}")
     print(f"Diabete: {misura_glicemia}")
     print(f"Pressione diastolica: {misura_pressione_diastolica }")
@@ -35,40 +32,32 @@ def submit_and_close():
     # Chiude la finestra
     root.destroy()
 
-# Creazione della finestra principale
 root = tk.Tk()
-root.title("Inserimento dati")
+root.title("Dati paziente")
 root.geometry("400x410")
 
 
-# Etichetta e campi di testo per i dati
-
-# Nome
+# Etichetta e campi per dato
 tk.Label(root, text="Inserisci nome e cognome:").pack(pady=5)
 nome_entry = ttk.Entry(root, width=30)
 nome_entry.pack(pady=5)
 
-# Diabete
 tk.Label(root, text="Inserisci valore Diabete:").pack(pady=5)
 diabete_entry = ttk.Entry(root, width=30)
 diabete_entry.pack(pady=5)
 
-# Pressione diastolica
 tk.Label(root, text="Inserisci Pressione Diastolica:").pack(pady=5)
 pressione_diastolica_entry = ttk.Entry(root, width=30)
 pressione_diastolica_entry.pack(pady=5)
 
-# Pressione sistolica
 tk.Label(root, text="Inserisci Pressione Sistolica:").pack(pady=5)
 pressione_sistolica_entry = ttk.Entry(root, width=30)
 pressione_sistolica_entry.pack(pady=5)
 
-# Casella di spunta
 checkbox_var = tk.BooleanVar()
 checkbox = ttk.Checkbutton(root, text="Ottimizza costo", variable=checkbox_var)
 checkbox.pack(pady=5)
 
-# Slider per la percentuale con freccette e inserimento manuale
 tk.Label(root, text="Salute del fegato e dei reni:").pack(pady=5)
 percentuale_var = tk.IntVar()
 percentuale_var.set(100)  # Imposta il valore iniziale a 100
@@ -82,40 +71,73 @@ submit_button.pack(pady=10)
 # Avvia il ciclo principale della finestra
 root.mainloop()
 
-if(misura_glicemia >= 90):                                                      #soglia di attivazione
-    E_d_min = np.maximum(0, misura_glicemia - 80) / 25      # 80 è consideato nominale
-else:
-    E_d_min = 0
-
-if (misura_pressione_sistolica >= 135 or  misura_pressione_diastolica >= 85):   #soglia di attivazione                                                               
-    E_h_min = (np.maximum(misura_pressione_sistolica - 130, 0) + np.maximum(misura_pressione_diastolica - 80, 0)) / 28.3    #
-else:
-    E_h_min = 0
-
-soglia_efficienza_minima = -0.5     #per non creare scompenso nelle efficienze
-
-null_solution = False
-
-if (E_h_min + E_d_min) != 0:
-    alpha = E_d_min/(E_h_min + E_d_min)
-    beta = E_h_min/(E_h_min + E_d_min)
-else:
-    null_solution = True
 
 
-# vincolo di carica farmacologica max
-k_max = (E_d_min + E_h_min)/3 * salute_fegato_reni_perc
 
-# ottimizzazione di spesa massima
+### CALCOLO E DEFINIZONE DI PARAMETRI, PESI, VINCOLI
 
+# parametri dell'ottimizzazione
+Ntot = 100                          #numero capienza database
+N = 20                              #numero farmaci utilizzati
+
+
+# costanti dell'ottimizzazione
+glicemia_nominale = 80
+glicemia_soglia_attivazione = 90
+coefficiente_di_scala_glicemia = 25
+
+pressione_diastolica_nominale = 80
+pressione_diastolica_soglia_attivazione = 90
+pressione_sistolica_nominale = 130
+pressione_sistolica_soglia_attivazione = 140
+coefficiente_di_scala_pressione = 28.3
+
+coefficiente_di_scala_carica = 3
+
+peso_ottimizzazione_costo = 0.005
+
+
+# (facoltativo per utente) ottimizzazione di spesa massima
 if ottimizza_costo :
-   Ro = 0.005
+   Ro = peso_ottimizzazione_costo       
 else :
    Ro = 0
 
 
+# calcolo euristico dei pesi dell'ottimizzazione, alpha e beta
+if(misura_glicemia >= glicemia_soglia_attivazione):                                                    
+    E_d_min = (misura_glicemia - glicemia_nominale) / coefficiente_di_scala_glicemia   
+else:
+    E_d_min = 0
 
-# Lettura del database dai file csv
+if (
+    misura_pressione_diastolica >= pressione_diastolica_soglia_attivazione      #alta pr. diastolica
+    or misura_pressione_sistolica >= pressione_sistolica_soglia_attivazione     #alta pr. sistolica
+   ):                                                                  
+    E_h_min = (np.maximum(misura_pressione_diastolica - pressione_diastolica_nominale, 0) 
+               + np.maximum(misura_pressione_sistolica - pressione_sistolica_nominale, 0)) / coefficiente_di_scala_pressione   
+else:
+    E_h_min = 0
+
+         
+if (E_h_min + E_d_min) != 0:                #peso livelli l'uno rispetto all'altro
+    alpha = E_d_min/(E_h_min + E_d_min)
+    beta = E_h_min/(E_h_min + E_d_min)
+else:                                       #se sono entrambi 0, l'algoritmo mi darà 0, non importa i pesi che assegno agli obiettivi
+    alpha = 1
+    beta = 1
+
+
+# vincolo di carica farmacologica max
+k_max = (E_d_min + E_h_min)/coefficiente_di_scala_carica * salute_fegato_reni_perc
+
+# vincolo di efficienza minima per evitare scompensi ad una patologia secondaria
+soglia_efficienza_minima = -0.5
+
+
+
+### LETTURA DEI DATABASE FILE CSV
+
 incompatibility_matrix = pd.read_csv('incompatibility_matrix.csv', header=None, skiprows=1, usecols=range(1, N+1)).to_numpy(dtype=int)
 
 drug_data = pd.read_csv('drug_data.csv', nrows=N)
@@ -129,14 +151,17 @@ sh_i = drug_data['sh_i'].to_numpy()             # Effetti collaterali per iperte
 nomi_farmaci = drug_data['DrugName'].to_numpy() # nomi farmaci     
 
 
+
+### ALGORITMO GENETICO
+
 # Crea lo spazio delle soluzioni
 creator.create("FitnessMax", base.Fitness, weights=(1.0,))  # Massimizza l'efficienza
 creator.create("Individual", list, fitness=creator.FitnessMax)
 toolbox = base.Toolbox()
 
 # Definizione di un individuo (soluzione)
-#toolbox.register("attr_int", np.random.randint, 0, X_i_Max.max() + 1)
-toolbox.register("attr_int", np.random.randint, 0, 1)
+#toolbox.register("attr_int", np.random.randint, 0, X_i_Max.max() + 1)  
+toolbox.register("attr_int", np.random.randint, 0, 1)                       #parte da soluzioni tutte nulle, più vicine al risultato corretto
 toolbox.register("individual", tools.initRepeat, creator.Individual, toolbox.attr_int, n=N)
 toolbox.register("population", tools.initRepeat, list, toolbox.individual)
 
@@ -160,10 +185,6 @@ def feasibility(individual):
     carica = np.sum(k_i * individual) 
     if carica > k_max:
         penalty += 1e6 * (carica - k_max)
-
-    # # Vincolo di budget
-    # if np.sum(c_i * individual) > budget:
-    #     penalty += 1e2
 
     # Vincolo di efficienza minima
     efficiency_d = np.sum((ed_i - sd_i) * individual)
@@ -194,22 +215,17 @@ toolbox.register("select", tools.selTournament, tournsize=3)
 # Algoritmo genetico
 toolbox.register("map", map)
 population = toolbox.population(n=200)
+algorithms.eaSimple(population, toolbox, cxpb=0.7, mutpb=0.2, ngen=1000, verbose=True)
+best_individual = tools.selBest(population, k=1)[0]                                     # Migliore soluzione
 
-if null_solution:
-    best_individual =  [0] * N
-else:
-    algorithms.eaSimple(population, toolbox, cxpb=0.7, mutpb=0.2, ngen=1000, verbose=True)
-    best_individual = tools.selBest(population, k=1)[0]                                     # Migliore soluzione
 
-# Migliore soluzione
-
+### PRESENTAZIONE DELLA SOLUZIONE
 print("\nMigliore soluzione:", best_individual)
 print("Efficienza su diabete:", np.sum((ed_i - sd_i) * best_individual))
 print("Efficienza su ipertensione:", np.sum((eh_i - sh_i) * best_individual))
 print("Efficienza totale:", np.sum((ed_i - sd_i + eh_i - sh_i) * best_individual))
 print("Carica totale:", np.sum(k_i * best_individual), " (target <=", k_max, ")" )
 print("Costo totale:", np.sum(c_i * best_individual) )
-
 
 print("\n\nPIANO TERAPEUTICO:")
 for i in range(0,19):
@@ -219,7 +235,9 @@ for i in range(0,19):
 print("\n\n")
 
 
-# generazione piano terapeutico in formato PDF
+### GENERAZIONE DEL PIANO TERAPEUTICO IN FORMATO PDF
+
+# generazione pdf
 output_ricetta = "Il/La signor/a " + nome_cognome + " a cura delle seguenti patologie di diabete e ipertensione, dovrà seguire giornalmente il seguente piano terapeutico: \n\n"
 for i in range(0,N):
     if best_individual[i] > 0:
@@ -232,9 +250,8 @@ titolo = "Piano terapeutico"
 pdf.set_font("Arial", style="B", size=16)  
 pdf.cell(200, 10, txt=titolo, ln=True, align='C')  
 pdf.ln(10) 
-
 pdf.set_font("Arial", size=12)  
-pdf.multi_cell(0, 10, txt=output_ricetta, align='L')  # Multi-cell per testo lungo
+pdf.multi_cell(0, 10, txt=output_ricetta, align='L') 
 
 pdf.output("piano.pdf")
 
